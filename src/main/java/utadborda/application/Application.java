a98f82c7b34cb8af4aeebf9ab12663447c9f713b
booninja <vav18@hi.is>
2020-11-26 05:09:21 +0000

    Sleppa að endurræsa databaseið

--- src/main/java/utadborda/application/Application.java
+++ src/main/java/utadborda/application/Application.java
@@ -15,11 +15,11 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx){
-		return args -> {
-
-		};
-	}
+//	@Bean
+//	public CommandLineRunner commandLineRunner(ApplicationContext ctx){
+//		return args -> {
+//
+//		};
+//	}
 
 }
