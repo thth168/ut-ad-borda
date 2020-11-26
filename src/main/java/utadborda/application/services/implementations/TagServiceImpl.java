801f401dc44faa4bbdcae724745107c52223b907
throstur <thth168@hi.is>
2020-11-26 05:51:12 +0000

    Some minor changes

--- src/main/java/utadborda/application/services/implementations/TagServiceImpl.java
+++ src/main/java/utadborda/application/services/implementations/TagServiceImpl.java
@@ -2,6 +2,7 @@ package utadborda.application.services.implementations;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import utadborda.application.Entities.Restaurant;
 import utadborda.application.Entities.Tag;
 import utadborda.application.services.DAO.TagRepo;
 import utadborda.application.services.TagService;
