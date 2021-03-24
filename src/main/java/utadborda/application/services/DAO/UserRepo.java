a4aa68706ef0d1904e01003df89bddba30026eea
throstur <thth168@hi.is>
2021-03-24 14:09:25 +0000

    Breyta stillingum fyrir heroku v2

--- src/main/java/utadborda/application/services/DAO/UserRepo.java
+++ src/main/java/utadborda/application/services/DAO/UserRepo.java
@@ -5,7 +5,7 @@ import utadborda.application.Entities.UAB_User;
 
 import java.util.UUID;
 
-public interface UserRepo extends JpaRepository<UAB_User, UUID> {
+public interface    UserRepo extends JpaRepository<UAB_User, UUID> {
     boolean existsByEmail(String email);
 
     UAB_User findByEmail(String email);
