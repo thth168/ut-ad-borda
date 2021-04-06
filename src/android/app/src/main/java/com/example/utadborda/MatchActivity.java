ae7bcf570e341686bc5f12cbe4030ed2e9469586
Mikolaj Cymcyk <mic5@hi.is>
2021-04-06 22:00:27 +0000

    gleymdi greinilega að publisha og committa 🙃🙃🙃

--- src/android/app/src/main/java/com/example/utadborda/MatchActivity.java
+++ src/android/app/src/main/java/com/example/utadborda/MatchActivity.java
@@ -10,11 +10,46 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 
+import java.util.List;
+
 public class MatchActivity extends AppCompatActivity {
+    List<String> userRightSwipes;
+    List<String> userLeftSwipes;
+    List<String> matchedIds;
+    Session session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match);
+        //session = new Session(session_key, this);
     }
+
+    private boolean isMatch(String restaurantId) {
+        if (matchedIds.contains(restaurantId)) {
+            return true;
+        }
+        return false;
+    }
+
+    private void recieve(List<String> restaurantIds) {
+        /*
+        if () {
+
+        } else {
+
+        }
+        */
+    }
+
+    private void onSwipe(boolean directionRight, String restaurantId) {
+        // session get new swipes from users
+        session.
+        // session notify swipe (direction)
+        if (directionRight) rightSwipes.add(restaurantId);
+        else leftSwipes.add(restaurantId);
+
+    }
+
+
 }
---
+++ src/android/app/src/main/java/com/example/utadborda/Session.java
@@ -0,0 +1,45 @@
+package com.example.utadborda;
+
+import org.json.JSONObject;
+
+public class Session {
+    String sessionKey;
+    MatchActivity matchActivity;
+
+    private Session(String key, MatchActivity matcher) {
+        sessionKey = key;
+        matchActivity = matcher;
+    }
+
+    private boolean notifyMembers(Object data) throws InterruptedException {
+        boolean notified;
+        try {
+            // notift all session members with data
+            // notified = x;
+            if (notified) return true;
+            else return false;
+        } catch (Exception e) {
+            System.out.println("Failed to notify session members.");
+        }
+        Thread.sleep(1000);
+        return notifyMembers(data, 0);
+    }
+
+    private boolean notifyMembers(Object data, int n) throws InterruptedException {
+        if (n < 5) {
+            boolean notified;
+            try {
+                // notify all session members with data
+                // notified = x;
+                if (notified) return true;
+                else return false;
+            } catch (Exception e) {
+                System.out.println("Failed to notify session members.");
+            }
+            return notifyMembers(data, n+1);
+            Thread.sleep(1000);
+        } else {
+            return false;
+        }
+    }
+}
